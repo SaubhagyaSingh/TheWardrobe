0c9e8277aea915b3dec7a8967476f8a38d8cc77b
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-10-25 03:01:20 +0530

    routes set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/navbar";
 import Tiles from "./Components/Tiles/Tiles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import arrivals from "./Pages/arrivals";
+import collection from "./Pages/collection";
+import login from "./Pages/login";
+import cart from "./Pages/cart";
+import product from "./Pages/product";
+
 function App() {
   return (
     <div>
-      <Navbar />
-      <Tiles />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<arrivals />} />
+          <Route path="/collections" element={<collection />} />
+          <Route path="/login" element={<login />} />
+          <Route path="/cart" element={<cart />} />
+          <Route path="/product" element={<product />}>
+            <Route path=":productId" element={<product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./navbar.css";
 import logo from "../Assets/logo.png";
+import { Link } from "react-router-dom";
+
 const navbar = () => {
   return (
     <div className="navbar">
@@ -9,10 +11,32 @@ const navbar = () => {
         <h1>The Wardrobe</h1>
       </div>
       <div className="Categories">
-        <li>New Arrivals</li>
-        <li>Cart</li>
-        <li>About Us</li>
-        <li>Login</li>
+        <ul>
+          <li>
+            <Link to="/">New Arrivals</Link>
+          </li>
+          <li>
+            <a href="#">Collections</a>
+            <ul className="cat">
+              <li>Suits</li>
+              <li>Dupatte</li>
+              <li>Bags</li>
+              <li>Sarees</li>
+              <li>Gowns</li>
+              <li>Skirts</li>
+              <li>Kurtis</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+          <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+          <Link to="/"></Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
